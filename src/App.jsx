dfdd62354059ee14e5b4686254ea0aed09173bcb
zerktea <kerere1999@gmail.com>
2023-02-23 21:40:12 +0100

    Header initiation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-
+import Header from './components/header'
 import './App.css'
 
 function App() {
 
   return (
     <div className="App">
-      
+      <Header/>
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import '../index.css'
+import { FaGithub } from 'react-icons/fa';
+export default function Header(){
+    return(
+        <div className="header">
+            <div className="header--elements">
+                <div className="header--img">
+                    <img src="/images/troll.png" alt="meme header" />
+                    <h4>Meme Generator</h4>
+                </div>
+                <div>
+                
+                    <h2> <a href="https://github.com/zerktea"> <FaGithub/></a> </h2>
+                </div>
+            </div>
+        </div>
+    )
+
+
+}
