a5a2b3493c9b218ed197c892cd7946c85a5ba804
zerktea <zakaria.beloufa13@gmail.com>
2023-03-05 20:43:25 +0100

    Api Fetching , useEffect, Form Intreactive

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,5 @@
 import '../index.css'
-import Memesdata from '../MemeData'
+// import Memesdata from '../MemeData'
 import React from 'react'
 export default function Form(){
     const [meme,setMeme]=React.useState({
@@ -7,13 +7,31 @@ export default function Form(){
         botomText : "",
         imageUrl : "http://i.imgflip.com/1bij.jpg"
     })
-    const [allMemeImages] = React.useState(Memesdata)
+    function handleForm(event){
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [event.target.name] : [event.target.value]
+        })
+        )
+    }
+
+
+
+    const [allMemeImages,setAllMemeImages] = React.useState()
+    
+    React.useEffect(function (){
+        fetch("https://api.imgflip.com/get_memes")
+         .then((response) => response.json())
+         .then((data) => setAllMemeImages(data.data.memes) )
+        
+     },[])
+    
     const [urlImg,setUrlImg]= React.useState()
     function getRandMeme(){
         setMeme(function(prevMeme){
             return {
                 ...prevMeme,
-                imageUrl: Memesdata.data.memes[Math.floor(Math.random()*Memesdata.data.memes.length)].url
+                imageUrl: allMemeImages[Math.floor(Math.random()*allMemeImages.length)].url
 
             };
         })
@@ -24,11 +42,13 @@ export default function Form(){
         <div className="meme">
             
                 
-            <input type="text" name='hellotoo' placeholder='Meme on Top' />
-            <input type="text" name="Hello" placeholder='Meme on Bottom' />
+            <input type="text" value={meme.topText} name='topText' placeholder='Meme on Top' onChange={handleForm}/>
+            <input type="text" value={meme.botomText}name="botomText" placeholder='Meme on Bottom' onChange={handleForm}/>
             <button className='form--btn button-3' type="submit" onClick={getRandMeme}>Generate Meme</button>
             <div className="meme--img">
                 <img src={meme.imageUrl} alt="soura" />  
+                <h3 className='top'>{meme.topText}</h3>
+                <h3 className='bot'>{meme.botomText}</h3>
             </div>
         </div>
     )
